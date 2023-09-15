67b3677f10b988ca511768ab2c7aed517f62a21b
KAEL <vincent.achy@gmail.com>
2023-09-15 16:35:20 +0200

    Add gitignore and env example

--- blog_backend/database.js
+++ blog_backend/database.js
@@ -1,13 +1,15 @@
 // database.js
 
-const { Pool } = require("pg");
+require('dotenv').config();
+
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: "myusername",
-  host: "localhost",
-  database: "blog_backend_db",
-  password: "mypassword",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
