fce4216a1b2127cb30259571f3258610fd85e999
KAEL <vincent.achy@gmail.com>
2023-11-02 16:55:04 +0100

    Suppression de fichiers inutiles

--- blog_backend/config/corsConfig.js
+++
@@ -1,9 +0,0 @@
-// corsConfig.js
-
-const corsConfig = {
-  origin: 'http://localhost:5173', // Adresse du frontend
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Méthodes HTTP autorisées
-  credentials: true, // Autorise les cookies et les en-têtes d'authentification
-};
-
-module.exports = corsConfig;
