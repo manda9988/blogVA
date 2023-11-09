bdcef7291ebc4f50a80a54084d89bfe460859519
KAEL <vincent.achy@gmail.com>
2023-11-09 15:36:25 +0100

    homeAndAccount

--- blogFront/src/pages/Account.svelte
+++ blogFront/src/pages/Account.svelte
@@ -1,40 +1,33 @@
 <!-- Account.svelte -->
-
 <script>
   // Importation des fonctions nécessaires de Svelte et svelte-spa-router
   import { onMount } from 'svelte';
   import { push } from 'svelte-spa-router';
-  import AutoLogout from '../lib/AutoLogout.svelte'; // <-- Ajout de l'importation
+  import AutoLogout from '../lib/AutoLogout.svelte';
 
-  // Initialisation des variables pour les articles et le nom d'utilisateur
   let articles = [];
   let username = localStorage.getItem('username');
   const API_URL = 'http://localhost:3002';
 
-  // Fonction exécutée lors du montage du composant
   onMount(async () => {
-    // Vérification si l'utilisateur est connecté
     if (!username) {
       alert('Veuillez vous connecter pour accéder à cette page.');
       push('/login');
       return;
     }
 
-    // Vérification de la validité du token
     const token = localStorage.getItem('token');
     if (!token) {
       push('/login');
       return;
     }
 
-    // Vérification du token avec le backend
     const response = await fetch(`${API_URL}/verifyToken`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
 
-    // Si le token n'est pas valide, déconnexion de l'utilisateur
     if (!response.ok) {
       localStorage.removeItem('username');
       localStorage.removeItem('token');
@@ -42,9 +35,8 @@
       return;
     }
 
-    // Récupération des articles de l'utilisateur
     const userId = localStorage.getItem('userId');
-    const userRole = localStorage.getItem('role'); // Supposons que vous stockiez également le rôle de l'utilisateur dans le localStorage
+    const userRole = localStorage.getItem('role');
 
     let articlesEndpoint = `${API_URL}/articles`;
     if (userRole !== 'admin') {
@@ -55,31 +47,28 @@
     articles = await res.json();
   });
 
-  // Fonction pour éditer un article
   function editArticle(id) {
     const confirmEdit = window.confirm(
       'Êtes-vous sûr de vouloir modifier cet article?',
     );
     if (confirmEdit) {
-      push(`/edit/${id}`); // Redirige vers la page d'édition
+      push(`/edit/${id}`);
     }
   }
 
-  // Fonction pour supprimer un article
   async function deleteArticle(id, title) {
     const confirmDelete = window.confirm(
       `Êtes-vous sûr de vouloir supprimer l'article ${title}?`,
     );
     if (confirmDelete) {
       const token = localStorage.getItem('token');
-      const res = await fetch(`http://localhost:3002/articles/${id}`, {
+      const res = await fetch(`${API_URL}/articles/${id}`, {
         method: 'DELETE',
         headers: {
           Authorization: `Bearer ${token}`,
         },
       });
 
-      // Mettre à jour la liste des articles après suppression
       if (res.ok) {
         articles = articles.filter((article) => article.id !== id);
       } else {
@@ -88,7 +77,7 @@
     }
   }
 
-  // Fonction pour se déconnecter
+  // MODIFICATION: Fonction pour se déconnecter
   function handleLogout() {
     const confirmLogout = window.confirm(
       'Êtes-vous sûr de vouloir vous déconnecter?',
@@ -98,23 +87,26 @@
     }
   }
 
-  // Fonction pour se déconnecter sans confirmation
+  // MODIFICATION: Fonction pour se déconnecter sans confirmation
   function logoutWithoutConfirmation() {
-    localStorage.removeItem('username'); // Supprime le nom d'utilisateur du stockage local
-    push('/'); // Redirige l'utilisateur vers la page d'accueil
+    console.log('Déconnexion de l’utilisateur'); // Log pour la confirmation de la déconnexion
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    // MODIFICATION: Assurez-vous d'effacer le rôle de l'utilisateur du localStorage
+    localStorage.removeItem('role');
+    push('/');
   }
 
-  // Fonction pour se désinscrire
   async function handleUnsubscribe() {
     const confirmUnsubscribe = window.confirm(
       'Êtes-vous sûr de vouloir vous désinscrire ? Cette action est irréversible.',
     );
     if (confirmUnsubscribe) {
-      const res = await fetch(`http://localhost:3002/users/${username}`, {
+      const res = await fetch(`${API_URL}/users/${username}`, {
         method: 'DELETE',
       });
 
-      // Se déconnecter après désinscription
       if (res.ok) {
         logoutWithoutConfirmation();
       } else {
@@ -171,5 +163,3 @@
     <button on:click={handleUnsubscribe}>Se désinscrire</button>
   </div>
 </div>
-
-<!-- Ce fichier est le composant de la page du compte utilisateur. Il affiche la liste des articles publiés par l'utilisateur avec des options pour éditer ou supprimer chaque article. Il offre également à l'utilisateur la possibilité de se déconnecter ou de se désinscrire. -->
--- blogFront/src/pages/Home.svelte
+++ blogFront/src/pages/Home.svelte
@@ -1,19 +1,58 @@
 <!-- Home.svelte -->
-
 <script>
-  // Importation des fonctions nécessaires de Svelte et svelte-spa-router
   import { onMount } from 'svelte';
   import { push } from 'svelte-spa-router';
-  import AutoLogout from '../lib/AutoLogout.svelte'; // <-- Ajout de l'importation
+  import AutoLogout from '../lib/AutoLogout.svelte';
 
-  // Initialisation d'un tableau vide pour stocker les articles
   let articles = [];
+  const API_URL = 'http://localhost:3002';
+  let users = []; // Définir users dans la portée globale du script
 
-  // Fonction exécutée lors du montage du composantt
   onMount(async () => {
-    // Récupération des articles depuis le backend
-    const res = await fetch('http://localhost:3002/articles');
-    articles = await res.json();
+    try {
+      const resUsers = await fetch(`${API_URL}/users`);
+      users = await resUsers.json();
+      console.log('Tous les utilisateurs:', users); // Débogage: Afficher tous les utilisateurs
+
+      const resArticles = await fetch(`${API_URL}/articles`);
+      const allArticles = await resArticles.json();
+      console.log('Tous les articles:', allArticles); // Débogage: Afficher tous les articles
+
+      const userRole = localStorage.getItem('role');
+      console.log('Rôle de l’utilisateur:', userRole); // Débogage: Afficher le rôle de l'utilisateur
+
+      if (!userRole || userRole === 'visitor') {
+        // Visiteur non connecté : Ne voir que les articles de l'admin
+        const adminUser = users.find((user) => user.role === 'admin');
+        console.log('Utilisateur admin:', adminUser); // Débogage: Afficher l'utilisateur admin
+
+        if (adminUser) {
+          articles = allArticles.filter(
+            (article) => article.user_id === adminUser.id,
+          );
+          console.log('Articles de l’admin:', articles); // Débogage: Afficher les articles de l'admin
+        } else {
+          console.error('Aucun utilisateur admin trouvé');
+        }
+      } else if (userRole === 'admin') {
+        articles = allArticles;
+      } else {
+        const userId = parseInt(localStorage.getItem('userId'));
+        // Utilise la variable users récupérée précédemment
+        const adminUser = users.find((user) => user.role === 'admin');
+        if (adminUser) {
+          articles = allArticles.filter(
+            (article) =>
+              article.user_id === userId || article.user_id === adminUser.id,
+          );
+          console.log('Articles visibles par l’utilisateur:', articles); // Débogage: Afficher les articles visibles par l'utilisateur
+        } else {
+          console.error('Aucun utilisateur admin trouvé');
+        }
+      }
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données:', error);
+    }
   });
 
   // Fonction pour naviguer vers la page d'un article spécifique
@@ -22,19 +61,16 @@
       event.type === 'click' ||
       (event.type === 'keydown' && event.key === 'Enter')
     ) {
-      console.log('Navigating to Article with ID:', id); // MODIFICATION: Log pour confirmer la navigation
       push(`/article/${id}`);
     }
   }
 </script>
 
 <AutoLogout />
-<!-- <-- Ajout du composant AutoLogout -->
 
 <!-- Section contenant la liste des articles -->
 <section>
   <div class="article-grid">
-    <!-- Boucle pour afficher chaque article -->
     {#each articles as article}
       <div
         class="article-card"
@@ -44,19 +80,16 @@
         on:keydown={(event) => navigateToArticle(article.id, event)}
       >
         <div class="content-container">
-          <!-- Affichage de l'image de l'article si elle existe -->
           {#if article?.imageurl}
             <div class="image-container">
               <img
-                src={`http://localhost:3002${article?.imageurl}`}
+                src={`${API_URL}${article?.imageurl}`}
                 alt={article?.title}
               />
             </div>
-            <!-- Affichage d'une image par défaut si l'article n'a pas d'image -->
           {:else}
             <div class="image-container default-bg"></div>
           {/if}
-          <!-- Titre et auteur de l'article -->
           <div class="title">{article.title}</div>
           <div class="author">Par {article.username}</div>
         </div>
@@ -64,5 +97,3 @@
     {/each}
   </div>
 </section>
-
-<!-- Ce fichier est le composant de la page d'accueil de l'application. Il affiche une liste d'articles récupérés depuis le backend. Chaque article est affiché sous forme de carte, et un clic sur une carte redirige l'utilisateur vers la page de l'article correspondant. -->
